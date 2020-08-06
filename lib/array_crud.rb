9a5123b3fea6aef69e87f694d04a467db841aaac
Youri Cadet <cadetjesusyouri@gmail.com>
2020-08-06 00:26:53 +0000

    Automatically backed up by Learn

---
+++ lib/array_crud.rb
@@ -0,0 +1,35 @@
+def create_an_empty_array
+  []
+end
+
+def create_an_array
+ youri_cadet = ["haiti", "delmas", "sud", "hello"]
+end
+
+def add_element_to_end_of_array(haiti, arrays)
+ youri_cadet << "arrays!"
+end
+
+def add_element_to_start_of_array(youri_cadet, element)
+youri_cadet.unshift("wow")
+end
+
+def remove_element_from_end_of_array(youri_cadet)
+arrays = cool_dogs.pop
+end
+
+def remove_element_from_start_of_array(cool_dogs)
+wow = cool_dogs.shift
+end
+
+def retrieve_element_from_index(array, element)
+ array[2]
+end
+
+def retrieve_first_element_from_array(cool_dogs)
+cool_dogs.first
+end
+
+def retrieve_last_element_from_array(cool_dogs)
+cool_dogs.last
+end
