d3e6eadfc0f398ad3db692c36ad7e272424085d0
Youri Cadet <cadetjesusyouri@gmail.com>
2020-08-06 00:38:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -2,12 +2,13 @@ def create_an_empty_array
   
 end
 
-def create_an_array(Youri)
-  Youri=
+def create_an_array
+  Youri=["name", "Last_name"]
   
 end
 
 def add_element_to_end_of_array(array, element)
+  
   
 end
 
